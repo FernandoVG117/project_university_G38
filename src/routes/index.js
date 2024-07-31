f87b365eb545d0d7cbabd7a14b615814b5042a19
Fernando <ing.fernandovg@gmail.com>
2024-07-31 13:26:35 -0600

    addition controller and router for Course

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const routerStudent = require('./student.router');
+const routerCourse = require('./course.router');
 const router = express.Router();
 
 // Put your routes here:
 router.use('/students', routerStudent);
+router.use('/courses', routerCourse);
 
 module.exports = router;
