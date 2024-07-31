5cb7ba66eaa129d554526b433a5118874f00a8b9
Fernando <ing.fernandovg@gmail.com>
2024-07-31 13:23:43 -0600

    create course model

---
+++ src/models/Course.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+
+const Course = sequelize.define('course', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    credits: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+});
+
+module.exports = Course;
