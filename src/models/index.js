318c9af91e821a74e8eb27e8ff7f6533c263bbc9
Fernando <ing.fernandovg@gmail.com>
2024-07-31 21:30:11 -0600

    make a special method courses_students

---
+++ src/models/index.js
@@ -0,0 +1,6 @@
+const Course = require("./Course");
+const Student = require("./Student");
+
+Course.belongsToMany(Student, {through: 'coursesStudents'})
+
+Student.belongsToMany(Course, {through: 'coursesStudents'})
